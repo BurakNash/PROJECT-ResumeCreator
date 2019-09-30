b25a86ad90883a937386fe29cdf180a8f00942a7
Burak Koc <burakkayakoc@gmail.com>
2019-09-30 07:51:57 -0500

    Experience model

---
+++ ResumeCreator/Models/Experience.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResumeCreator.Models
+{
+    public class Experience
+    {
+        public int ID { get; set; }
+        public string StartDate { get; set; }
+        public string StopDate { get; set; }
+        public string JobName { get; set; }
+        public string CompanyName { get; set; }
+        public string Where { get; set; }
+        public string Explanation { get; set; }
+        public bool İsContinue { get; set; }
+
+    }
+}
