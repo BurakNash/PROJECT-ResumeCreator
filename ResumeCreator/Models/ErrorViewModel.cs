a7330b0f573d59360907c642952a0e701e81ebd3
Burak Koc <burakkayakoc@gmail.com>
2019-09-30 07:46:29 -0500

    base mvc

---
+++ ResumeCreator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResumeCreator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
