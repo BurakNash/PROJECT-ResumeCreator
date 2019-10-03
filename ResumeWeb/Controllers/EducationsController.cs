a7a7581f7009e9336d559b74508ca38d14f868a7
Burak Koc <burakkayakoc@gmail.com>
2019-10-03 05:23:33 -0500

    Education controller added

---
+++ ResumeWeb/Controllers/EducationsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ResumeWeb.Models;
+
+namespace ResumeWeb.Controllers
+{
+    public class EducationsController : Controller
+    {
+        private ModelMigrations db = new ModelMigrations();
+
+        // GET: Educations
+        public ActionResult Index()
+        {
+            return View(db.Educations.ToList());
+        }
+
+        // GET: Educations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education education = db.Educations.Find(id);
+            if (education == null)
+            {
+                return HttpNotFound();
+            }
+            return View(education);
+        }
+
+        // GET: Educations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,StartDate,StopDate,IsContinue,SchoolName,Departmant,Where,Explanation")] Education education)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Educations.Add(education);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(education);
+        }
+
+        // GET: Educations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education education = db.Educations.Find(id);
+            if (education == null)
+            {
+                return HttpNotFound();
+            }
+            return View(education);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,StartDate,StopDate,IsContinue,SchoolName,Departmant,Where,Explanation")] Education education)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(education).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(education);
+        }
+
+        // GET: Educations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education education = db.Educations.Find(id);
+            if (education == null)
+            {
+                return HttpNotFound();
+            }
+            return View(education);
+        }
+
+        // POST: Educations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Education education = db.Educations.Find(id);
+            db.Educations.Remove(education);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
