37fb6952a76a59c9849b03c0c21ef7c23b71813c
Burak Koc <burakkayakoc@gmail.com>
2019-10-08 20:07:39 -0500

    Projects modal created

---
+++ ResumeWeb/Models/Projects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ResumeWeb.Models
+{
+    public class Projects
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+    }
+}
